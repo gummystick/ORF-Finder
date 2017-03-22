2f92cc9f4a9c7fde91029b31a244fecb78543178
gummystick <willysieswilly@gmail.com>
2017-03-22 10:33:20 +0100

    Adding of different classes: Inlezen, ORFObject, TransportData, Verwerken

---
+++ ORF Finder Application/src/orf/finder/application/Inlezen.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orf.finder.application;
+
+/**
+ *
+ * @author William
+ */
+public class Inlezen {
+    
+}
