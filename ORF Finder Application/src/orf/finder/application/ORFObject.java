1b60a604a8a332462508a6e5a9ebc7f9ed779e50
gummystick <willysieswilly@gmail.com>
2017-03-23 13:42:23 +0100

    ORF_ID, DNASequence, positie en percentageGC toegevoegd als variabelen. Constructor aangemaakt, verschillende methoden voor het opslaan en ophalen van de variabelen toegevoegd.

--- ORF Finder Application/src/orf/finder/application/ORFObject.java
+++ ORF Finder Application/src/orf/finder/application/ORFObject.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,4 +11,32 @@ package orf.finder.application;
  */
 public class ORFObject {
     
+    protected String orfID;
+    protected String dnaSequence;
+    protected long positie;//could be another kind of variable, depending on the position notation
+    protected double percentageGC;
+    protected String test;
+
+    
+    public ORFObject(){
+        
+    }
+    
+    private void setDnaSequence(String sequence){
+        dnaSequence = sequence;
+    }
+    
+    private String getDnaSequence(){
+        return dnaSequence;
+    }
+    
+    private long getPositie(){
+        return positie;
+    }
+    
+    private double getPercentageGC(){
+        return percentageGC;
+    }
+}
+
 }
