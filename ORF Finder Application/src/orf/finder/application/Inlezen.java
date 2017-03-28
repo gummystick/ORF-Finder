14d683d585f4c6767415c2595d47a9e135a8a229
User <User@145.74.58.65>
2017-03-28 12:23:16 +0200

    Inlezen

--- ORF Finder Application/src/orf/finder/application/Inlezen.java
+++ ORF Finder Application/src/orf/finder/application/Inlezen.java
@@ -5,11 +5,39 @@
  */
 package orf.finder.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author William
  */
 public class Inlezen {
-    private String test;
     
+    public void readFile() {
+        
+        BufferedReader br = null;
+        String path = "C:\\Users\\User\\Documents\\Course 7\\Weektaken\\Debaryomyces_occidentalis.fas";
+        try {
+            br = new BufferedReader(new FileReader(path));
+            br.readLine();
+            
+            String line;
+            while ((line = br.readLine()) != null) {
+               
+            }
+            br.close();
+        
+        } catch (IOException ioe) {
+         JOptionPane.showMessageDialog(null,
+                    "File Error: " + ioe.toString());
+        } 
+        catch (NullPointerException np){
+            System.out.println("Er is een fout opgetreden");
+        }
+        catch (Exception ex) {
+            System.out.println("Onbekende fout: raadpleeg uw systeembeheerder");} 
+    }
 }
