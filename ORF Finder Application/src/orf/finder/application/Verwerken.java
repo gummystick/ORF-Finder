1e0084568ec1ef2cebff8b8fa18055c028c3ee6e
gabeplz <Gebruiker@145.102.198.158>
2017-03-31 22:35:20 +0200

    klaar zet geef de arraylist door in static void verwerk() onder  reading frane 6

--- ORF Finder Application/src/orf/finder/application/Verwerken.java
+++ ORF Finder Application/src/orf/finder/application/Verwerken.java
@@ -15,12 +15,8 @@ import java.util.regex.Pattern;
  * @author Gebruiker
  */
 public class Verwerken {
-private static ArrayList<String> gen = new ArrayList<String>();
-private static String header = "abc";
-                                //"876543210987654321
-                                //"AATGTAA"
-                                //"TTTTTACATT"
-private static String genoomSeq = "TTACAT"; //"xxxATGdxxxxxbdxxxxxxxxbTAGxxxxxxxxxxxxATGddddddbTAGxxxxxxxxx";
+
+private static String genoomSeq;
 private static String reverseCompGenoomSeq;
 private static String codonRead;
 private static String[] genoomRfArray1;
@@ -31,7 +27,7 @@ private static String[] genoomRfArray5;
 private static String[] genoomRfArray6;
 private static String[] startCodon = {"ATG"};
 private static String[] stopCodons = {"TAA", "TAG", "TGA"};
-private static ArrayList<Integer> reReadPositions = new ArrayList<Integer>();
+private static ArrayList<ORFObject> ORFObjectArray = new ArrayList<ORFObject>();
 
 private static boolean startFound = false;
 private static boolean searchOnlyStop = false;
@@ -41,23 +37,32 @@ private static int positieStart;
 //private static
 //private static String[] stopCodons = {"123", "456", "789"};
 
-
-    public static void verwerk(){
-            gen.add(header);
-            gen.add(genoomSeq); 
-               
+    /**
+     *haalt de de data van genoomseq uit de arraylist sequentieInfo en roept de andere
+     * methodes in deze class aan
+     * @param sequentieInfo
+     */
+    public static void verwerk(ArrayList<ArrayList> sequentieInfo){
+            
+            genoomSeq = sequentieInfo.get(1).get(0).toString().toUpperCase();               
             makeReverseComp();
-            //verwerkReadingFrame1();
-           // verwerkReadingFrame2();
-           // verwerkReadingFrame3();
-           verwerkReadingFrame4();
-           //           verwerkReadingFrame5();
+            verwerkReadingFrame1();                                             
+            verwerkReadingFrame2();
+            verwerkReadingFrame3();
+            verwerkReadingFrame4();
+            verwerkReadingFrame5();
+            verwerkReadingFrame6();
+                        
            
     }
-    
-    public static void makeReverseComp(){
-    
-    StringBuilder builder = new StringBuilder(genoomSeq);
+                                             
+    /**
+     * keert de genoomSeq string om ver verwisseld elk voorkomen van A en T met elkaar 
+     * en elk voorkomen van C en G
+     */
+    public static void makeReverseComp(){                                       //  
+                                                                                // 
+    StringBuilder builder = new StringBuilder(genoomSeq);                       //met elkaar
     
     reverseCompGenoomSeq = builder.reverse().toString();
    
@@ -69,8 +74,15 @@ private static int positieStart;
     
     }
     
-    public static void verwerkReadingFrame1(){    
-        
+    /**
+     * splits de genoomSeq in in substrings van 3 tekens groot
+     * begint bij index 0 op de genoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
+     */
+    public static void verwerkReadingFrame1(){                          
+        startFound = false;                                               
+        searchOnlyStop = false;
         genoomRfArray1 = new String[genoomSeq.length()/3];
         for (int x = 0; x<genoomSeq.length()/3 ; x++) {
             genoomRfArray1[x] = genoomSeq.substring(0 + x*3, 3 + x*3);
@@ -80,20 +92,18 @@ private static int positieStart;
                 if (searchOnlyStop == false){
                 positieStart = x * 3;    
                 startFound = true;
-                System.out.println(x * 3 + 1 + "start op hele genoom");
                 searchOnlyStop = true;
                 } 
             }   
             if (startFound == true){
                 for (int i = 0; i<stopCodons.length; i++){
                     if (genoomRfArray1[x].equals(stopCodons[i])){
-                        OrfObject orf = new OrfObject();
+                        ORFObject orf = new ORFObject();
                         orf.setStartPositie(positieStart + 1);
                         orf.setStopPositie(x * 3 + 3);
-                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 3));                   
-                        System.out.println(positieStart + 1 + "start1 op reading frame" );
-                        System.out.println(x * 3 + 3 + "stop1 op reading frame");
-                        System.out.println(genoomSeq.substring(positieStart, x * 3 + 3));
+                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 3));    
+                        orf.setReadingFrame(1);
+                        ORFObjectArray.add(orf);
                         startFound = false;
                         searchOnlyStop = false;                       
                     }
@@ -102,8 +112,15 @@ private static int positieStart;
         }
     }                   
     
+    /**
+     * splits de genoomSeq in in substrings van 3 tekens groot
+     * begint bij index 1 op de genoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
+     */
     public static void verwerkReadingFrame2(){    
-        
+        startFound = false;
+        searchOnlyStop = false;
         genoomRfArray2 = new String[(genoomSeq.length()-1)  /3 ];
         for (int x = 0; x<(genoomSeq.length()-1)/3  ; x++) {
             genoomRfArray2[x] = genoomSeq.substring(1 + x*3, 4 + x*3);
@@ -113,20 +130,18 @@ private static int positieStart;
                     if(genoomRfArray2[x].equals(startCodon[0])) {            
                     positieStart = x * 3 + 1;    
                     startFound = true;
-                    System.out.println(x * 3 + 2 + "start op hele genoom");
                     searchOnlyStop = true;
                 }
             }
             if (startFound == true){
                 for (int i = 0; i<stopCodons.length; i++){
                     if (genoomRfArray2[x].equals(stopCodons[i])){
-                        OrfObject orf = new OrfObject();
+                        ORFObject orf = new ORFObject();
                         orf.setStartPositie(positieStart + 1);
                         orf.setStopPositie(x * 3 + 3 + 1);
-                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 4));                   
-                        System.out.println(positieStart + 1 +"start3" );
-                        System.out.println(x * 3 + 3 + 1 + "stop4");
-                        System.out.println(genoomSeq.substring(positieStart, x * 3 + 4));
+                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 4));    
+                        orf.setReadingFrame(2);
+                        ORFObjectArray.add(orf);
                         startFound = false;
                         searchOnlyStop = false;                       
 
@@ -136,9 +151,15 @@ private static int positieStart;
         }
     }
     
-    
+    /**
+     * splits de genoomSeq in in substrings van 3 tekens groot
+     * begint bij index 2 op de genoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
+     */
     public static void verwerkReadingFrame3(){    
-        
+        startFound = false;
+    searchOnlyStop = false;
         genoomRfArray3 = new String[(genoomSeq.length()-2)  /3 ];
         for (int x = 0; x<(genoomSeq.length()-2)/3  ; x++) {
             genoomRfArray3[x] = genoomSeq.substring(2 + x*3, 5 + x*3);
@@ -148,20 +169,18 @@ private static int positieStart;
                 if(genoomRfArray3[x].equals(startCodon[0])) {
                     positieStart = x * 3 + 2;    
                     startFound = true;
-                    System.out.println(x * 3 + 3 + "start op hele genoom");
                     searchOnlyStop = true;
                 }
             }
             if (startFound == true){
                 for (int i = 0; i<stopCodons.length; i++){
                     if (genoomRfArray3[x].equals(stopCodons[i])){
-                        OrfObject orf = new OrfObject();
+                        ORFObject orf = new ORFObject();
                         orf.setStartPositie(positieStart + 1);
                         orf.setStopPositie(x * 3 + 3 + 2);
-                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 5));                   
-                        System.out.println(positieStart + 1 +"start3" );
-                        System.out.println(x * 3 + 3 + 2 + "stop4");
-                        System.out.println(genoomSeq.substring(positieStart, x * 3 + 5));
+                        orf.setORFseq(genoomSeq.substring(positieStart, x * 3 + 5));
+                        orf.setReadingFrame(3);
+                        ORFObjectArray.add(orf);
                         startFound = false;
                         searchOnlyStop = false;                       
 
@@ -171,8 +190,15 @@ private static int positieStart;
         }
     }
     
+    /**
+     * splits de reverseCompGenoomSeq in in substrings van 3 tekens groot
+     * begint bij index 0 op de reverseCompGenoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
+     */
     public static void verwerkReadingFrame4(){    
-                                            
+             startFound = false;
+    searchOnlyStop = false;                               
         genoomRfArray4 = new String[reverseCompGenoomSeq.length()/3];
         for (int x = 0; x<reverseCompGenoomSeq.length()/3 ; x++) {
             genoomRfArray4[x] = reverseCompGenoomSeq.substring(0 + x*3, 3 + x*3);
@@ -182,7 +208,6 @@ private static int positieStart;
                 if(genoomRfArray4[x].equals(startCodon[0])) {
                 positieStart = x * 3;    
                 startFound = true;
-                System.out.println(x * 3 + 1 + "start op hele genoom");
                 searchOnlyStop = true;
                 } 
             }   
@@ -190,15 +215,12 @@ private static int positieStart;
 
                 for (int i = 0; i<stopCodons.length; i++){
                     if (genoomRfArray4[x].equals(stopCodons[i])){
-                        OrfObject orf = new OrfObject();
-                        orf.setStartPositie(reverseCompGenoomSeq.length() -positieStart );
-                        orf.setStopPositie(reverseCompGenoomSeq.length() -x * 3 -2);
-                        orf.setORFseq(reverseCompGenoomSeq.substring(positieStart, x * 3 + 3));                   
-                        System.out.println(positieStart + 1 + "start1 op reading frame" );
-                        System.out.println(x * 3 + 3 + "stop1 op reading frame");
-                        System.out.println(reverseCompGenoomSeq.substring(positieStart, x * 3 + 3));
-                        System.out.println(reverseCompGenoomSeq.length() -positieStart );
-                        System.out.println(reverseCompGenoomSeq.length() -x * 3 -2);
+                        ORFObject orf = new ORFObject();
+                        orf.setStartPositie(reverseCompGenoomSeq.length() -x * 3 -2);                        
+                        orf.setStopPositie(reverseCompGenoomSeq.length() -positieStart);
+                        orf.setORFseq(reverseCompGenoomSeq.substring(positieStart, x * 3 + 3));     
+                        orf.setReadingFrame(-1);
+                        ORFObjectArray.add(orf);
                         startFound = false;
                         searchOnlyStop = false;                       
                     }
@@ -206,8 +228,16 @@ private static int positieStart;
             }
         }
     }                   
+
+    /**
+     * splits de reverseCompGenoomSeq in in substrings van 3 tekens groot
+     * begint bij index 1 op de reverseCompGenoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
+     */
     public static void verwerkReadingFrame5(){    
-        
+        startFound = false;
+    searchOnlyStop = false;
         genoomRfArray5 = new String[(reverseCompGenoomSeq.length()-1)  /3 ];
         for (int x = 0; x<(reverseCompGenoomSeq.length()-1)/3  ; x++) {
             genoomRfArray5[x] = reverseCompGenoomSeq.substring(1 + x*3, 4 + x*3);
@@ -217,21 +247,18 @@ private static int positieStart;
                     if(genoomRfArray5[x].equals(startCodon[0])) {            
                     positieStart = x * 3 + 1;    
                     startFound = true;
-                    System.out.println(x * 3 + 2 + "start op hele genoom");
                     searchOnlyStop = true;
                 }
             }
             if (startFound == true){
                 for (int i = 0; i<stopCodons.length; i++){
                     if (genoomRfArray5[x].equals(stopCodons[i])){
-                        OrfObject orf = new OrfObject();
-                        orf.setStartPositie(reverseCompGenoomSeq.length() -positieStart);
-                        orf.setStopPositie(reverseCompGenoomSeq.length() -x * 3 -3 -1);
-                        orf.setORFseq(reverseCompGenoomSeq.substring(positieStart, x * 3 + 4));                   
-                        System.out.println(reverseCompGenoomSeq.length() -positieStart +"start3" );
-                        System.out.println(reverseCompGenoomSeq.length() -x * 3 -3  + "stop3");
-                        //System.out.println(x * 3 + 3 + 1 + "stop4");
-                        System.out.println(reverseCompGenoomSeq.substring(positieStart, x * 3 + 4)); // 13 22  tot28
+                        ORFObject orf = new ORFObject();
+                        orf.setStartPositie(reverseCompGenoomSeq.length() -x * 3 -3);                        
+                        orf.setStopPositie(reverseCompGenoomSeq.length() -positieStart);
+                        orf.setORFseq(reverseCompGenoomSeq.substring(positieStart, x * 3 + 4));
+                        orf.setReadingFrame(-2);
+   
                         startFound = false;
                         searchOnlyStop = false;                       
 
@@ -240,28 +267,51 @@ private static int positieStart;
             }
         }
     }
-    
-   //     System.out.println(genoomSeq.substring(x*3, x * 3 + 3));
-    
-    
-   // String dna = "xxxATGdxxxxxbdxxxxxxxxbTAGxxxxxxxxx";
 
-            
-    /*genes.add(m.group().);
-    m.group().replaceAll(m.group(), "");
-    
-    for (int x = 0; x<genes.size(); x++){
-    System.out.println(genes.get(0) + x);
-    }
-    */
-    
-
-    
     /**
-     * @param args the command line arguments
+     * splits de reverseCompGenoomSeq in in substrings van 3 tekens groot
+     * begint bij index 2 op de reverseCompGenoomSeq
+     * zodra een stop condon na een start codon is gevonden word 
+     * een ORFObject aangemaakt
      */
-    public static void main(String[] args) {
-       verwerk();
-    }
+    public static void verwerkReadingFrame6(){    
+        startFound = false;
+        searchOnlyStop = false;
+    
+        genoomRfArray6 = new String[(reverseCompGenoomSeq.length()-2)  /3 ];
+        for (int x = 0; x<(reverseCompGenoomSeq.length()-2)/3  ; x++) {
+            genoomRfArray6[x] = reverseCompGenoomSeq.substring(2 + x*3, 5 + x*3);
+        } 
+        System.out.println("1");
+        for (int x = 0; x<genoomRfArray6.length; x++){
+                if (searchOnlyStop == false){
+                    if(genoomRfArray6[x].equals(startCodon[0])) {            
+                    positieStart = x * 3 + 2;    
+                    startFound = true;
+                    searchOnlyStop = true;
+                }
+            }System.out.println("2");
+            if (startFound == true){
+                for (int i = 0; i<stopCodons.length; i++){
+                    if (genoomRfArray6[x].equals(stopCodons[i])){
+                        ORFObject orf = new ORFObject();
+                        orf.setStartPositie(reverseCompGenoomSeq.length() -x * 3 -4);                        
+                        orf.setStopPositie(reverseCompGenoomSeq.length() -positieStart);
+                        orf.setORFseq(reverseCompGenoomSeq.substring(positieStart, x * 3 + 5));  
+                        orf.setReadingFrame(-3);
+                        System.out.println("3");
+                        startFound = false;
+                        searchOnlyStop = false;                       
+
+                    }
+                }
+            }
+        }
+    }    
+ 
+    
+
+    
+   
     
 }
